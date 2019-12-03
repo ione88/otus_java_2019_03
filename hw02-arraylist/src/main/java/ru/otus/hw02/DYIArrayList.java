c025c8689bbe8a1304ecc4f2730c2ea17a30ab9c
Ivan Alekseev <programinvest@gmail.com>
2019-12-03 18:10:02 +0300

    add implement some methods in progress

--- hw02-arraylist/src/main/java/ru/otus/hw02/DYIArrayList.java
+++ hw02-arraylist/src/main/java/ru/otus/hw02/DYIArrayList.java
@@ -1,45 +1,81 @@
 package ru.otus.hw02;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class DYIArrayList<E> implements List<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final Object[] EMPTY_VALUES = {};
+    transient Object[] values; // non-private to simplify nested class access
+    private int length;
+
+    public DYIArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.values = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.values = EMPTY_VALUES;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    initialCapacity);
+        }
+    }
+
+    public DYIArrayList() {
+        this.values = EMPTY_VALUES;
+    }
 
     @Override
     public int size() {
-        return 0;
+        return length;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return length == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        throw new UnsupportedOperationException("contains");
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        throw new UnsupportedOperationException("iterator");
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        throw new UnsupportedOperationException("toArray");
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        throw new UnsupportedOperationException("<T> toArray");
+    }
+
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = values.length;
+        if (oldCapacity > 0 || values != EMPTY_VALUES) {
+            int newCapacity = ArraysSupport.newLength(oldCapacity,
+                    minCapacity - oldCapacity, /* minimum growth */
+                    oldCapacity >> 1           /* preferred growth */);
+            return values = Arrays.copyOf(values, newCapacity);
+        } else {
+            return values = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
+        }
+    }
+
+    private void add(E e, Object[] values, int s) {
+        if (s == values.length)
+            values = grow(size() + 1);
+        values[s] = e;
+        length = s + 1;
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        add(e, values, length);
+        return true;
     }
 
     @Override
@@ -74,12 +110,23 @@ public class DYIArrayList<E> implements List<E> {
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < length; i++)
+            values[i] = null;
+        length = 0;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if (index < size())
+            return valueData(index);
+        throw new ArrayIndexOutOfBoundsException("index more then size" + index + " > " + size());
+    }
+
+    // Positional Access Operations
+
+    @SuppressWarnings("unchecked")
+    E valueData(int index) {
+        return (E) values[index];
     }
 
     @Override
